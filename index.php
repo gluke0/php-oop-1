0c5afb2fc3b4675fde9b21dc3f39d9b50fc0fde7
gluke0 <milanesigianluca@gmail.com>
2023-05-31 14:46:58 +0200

    construct

--- index.php
+++ index.php
@@ -5,7 +5,21 @@ class Movie{
     public $year;
     public $length;
     public $genre;
-    public $IMDBrating
+    public $IMDBrating;
+
+    function __construct($title, $year, $length, $genre, $IMDBrating,){
+        $this-> title = $title;
+        $this-> year = $year;
+        $this-> length = $length;
+        $this-> genre = $genre;
+        $this-> IMDBrating = $IMDBrating;
+
+    }
+
 }
+
+$gladiatore = new Movie ('gladiatore', '2000', '300min', 'action', '4/5');
+
+var_dump($gladiatore);
 
 ?>
