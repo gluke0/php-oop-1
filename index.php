040b6f8e1f74bf7c8eced90e43c5e49d1be1a8b5
gluke0 <milanesigianluca@gmail.com>
2023-05-31 15:09:20 +0200

    medoto per voto

--- index.php
+++ index.php
@@ -7,19 +7,26 @@ class Movie{
     public $genre;
     public $IMDBrating;
 
+    public function setIMDBrating(){
+        $this-> IMDBrating = rand(0, 5);
+    }
+
     function __construct($title, $year, $length, $genre, $IMDBrating,){
         $this-> title = $title;
         $this-> year = $year;
         $this-> length = $length;
         $this-> genre = $genre;
-        $this-> IMDBrating = $IMDBrating;
+        $this-> setIMDBrating();
 
     }
 
 }
 
-$gladiatore = new Movie ('gladiatore', '2000', '300min', 'action', '4/5');
+$gladiatore = new Movie ('gladiatore', '2000', '300min', 'action', 'setIMDBrating');
 
 var_dump($gladiatore);
 
+$pokemon = new Movie ('Pokemon', '1992', '120min', 'cartoon', 'setIMDBrating');
+
+var_dump($pokemon);
 ?>
